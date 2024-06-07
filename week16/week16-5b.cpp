870f63b174fb74503404a88291e3bbbdeb6e369c
towa-is-tmt <jefferytcc13@gmail.com>
2024-06-07 11:56:56 +0800

    week16

---
+++ week16/week16-5b.cpp
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	int a[7] = {};
+	char s[200];
+	scanf("%s",s);
+	int N = strlen(s);
+	for(int i=0; i<N; i++){
+		for(int d=0; d<=6; d++){
+			if(s[i]=='0'+d) a[d]++;
+		}
+	}
+	for(int d=1; d<=6;d++){
+		printf("%d:%d\n", d,a[d]);
+	}
+
+
+}
---
+++ week16/week16-5c.cpp
@@ -0,0 +1,11 @@
+
+
+#include <stdio.h>
+int main()
+{
+	char a[200];
+
+	scanf("%s", &a);
+	printf("%c", a[0]);
+
+}
